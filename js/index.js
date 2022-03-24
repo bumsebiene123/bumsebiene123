7d12d113f13d0e845c04430d20fadd25227d8bdc
bumsebiene123 <102155255+bumsebiene123@users.noreply.github.com>
2022-03-24 12:55:34 +0100

    Delete js directory

--- js/index.js
+++
@@ -1,7 +0,0 @@
-const $contactBtn = document.querySelector("#contact");
-const $modal = document.querySelector(".modal_container");
-const $flex = document.querySelector(".flex");
-$contactBtn.addEventListener("click", () => {
-  $modal.classList.toggle("active");
-  $flex.classList.toggle("modal-active");
-});
